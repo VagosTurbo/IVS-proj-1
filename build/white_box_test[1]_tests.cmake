743624d246def949e54ec8cbf14d6b1c121126dd
Boris Semanco <semanco.boris@gmail.com>
2023-03-06 18:54:39 +0100

    tried sth in white box and also updated tdd code

--- build/white_box_test[1]_tests.cmake
+++ build/white_box_test[1]_tests.cmake
@@ -1,3 +1,5 @@
 add_test( BasicTests.TableConstructor [==[/home/boriss/Documents/VUT FIT/IVS/proj1/build/white_box_test]==] [==[--gtest_filter=BasicTests.TableConstructor]==] --gtest_also_run_disabled_tests)
 set_tests_properties( BasicTests.TableConstructor PROPERTIES WORKING_DIRECTORY [==[/home/boriss/Documents/VUT FIT/IVS/proj1/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( white_box_test_TESTS BasicTests.TableConstructor)
+add_test( BasicTests.TableDestructor [==[/home/boriss/Documents/VUT FIT/IVS/proj1/build/white_box_test]==] [==[--gtest_filter=BasicTests.TableDestructor]==] --gtest_also_run_disabled_tests)
+set_tests_properties( BasicTests.TableDestructor PROPERTIES WORKING_DIRECTORY [==[/home/boriss/Documents/VUT FIT/IVS/proj1/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set( white_box_test_TESTS BasicTests.TableConstructor BasicTests.TableDestructor)
